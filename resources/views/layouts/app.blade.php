ee5b4cd08234e85c27639cf03011cde215f9a81d
Emilien Charpié <emilien.charpie@gmail.com>
2024-04-17 13:37:55 +0200

    name of the app changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Immage Classification</title>
+        <title>Vision - Visualize Your World</title>
         <meta name="description" content="Image clssification with AI" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
